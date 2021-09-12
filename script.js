0f82f68c19f030f928c1a46f3db6a00bc5286b28
Kokorina Ekaterina <kat3110@mail.ru>
2021-09-12 19:22:26 +0300

    js

--- script.js
+++ script.js
@@ -27,12 +27,71 @@ openPopupButton[0].addEventListener("click", openPopup, false);
 let closePopupButton = document.getElementsByClassName("popup__close");
 closePopupButton[0].addEventListener("click", closePopup, false);
 
-// // сохранение имени и описания
-function change() {
+
+// сохранение имени и описания
+function changePopup() {
+    // определяем значение полей ввода имени и описания
     let changeName = document.querySelector(".popup__input_text_name").value
     let changeDescription = document.querySelector(".popup__input_text_description").value
+
+    // определяем поля куда вводим новые данные
     let name = document.querySelector(".profile__name");
     let description = document.querySelector(".profile__description");
-    name.innerHTML = changeName;
-    description.innerHTML = changeDescription;
+
+
+    // проверка на введенный текст
+    if (changeName == "") {
+        // если текста нет, то ничего не происходит
+    } else {
+        // меняем имя
+        name.innerHTML = changeName;
+
+        // очищаем поля ввода имени
+        document.querySelector(".popup__input_text_name").value = "";
+
+        // меняем плейсхолдер у поля ввода имени
+        document.querySelector(".popup__input_text_name").placeholder = changeName;
+    }
+
+    // проверка на введенный текст
+    if (changeDescription == "") {
+        // если текста нет, то ничего не происходит
+    } else {
+        // меняем описание
+        description.innerHTML = changeDescription;
+
+        // очищаем поля ввода описания
+        document.querySelector(".popup__input_text_description").value = "";
+
+        // меняем плейсхолдер у поля ввода описания
+        document.querySelector(".popup__input_text_description").placeholder = changeDescription;
+    }
+
+    // закрываем попап
+    closePopup()
 }
+
+// кнопка сохранить + listener
+let savePopupButton = document.getElementsByClassName("popup__save");
+savePopupButton[0].addEventListener("click", changePopup, false);
+
+
+// лайк
+function clickLike() {
+    // определяем иконку лайк
+    let like = document.querySelector(".elements__like");
+
+    // делаем проверку на наличие класса актив
+    if (like.classList.contains("elements__active") === true) {
+        // если класс есть, удаляем его
+        like.classList.remove("elements__active");
+    } else {
+        // если класса нет, добавляем его
+        like.classList.add("elements__active");
+    }
+
+}
+
+// нажатие на клик
+let likeButton = document.getElementsByClassName("elements__like");
+likeButton[0].addEventListener("click", clickLike, false);
