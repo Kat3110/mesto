7de06997e3da9787bd17c7ef6b4a16cfeb8b53e5
Kokorina Ekaterina <kat3110@mail.ru>
2021-09-13 02:08:34 +0300

    like

--- script.js
+++ script.js
@@ -27,46 +27,36 @@ openPopupButton[0].addEventListener("click", openPopup, false);
 let closePopupButton = document.getElementsByClassName("popup__close");
 closePopupButton[0].addEventListener("click", closePopup, false);
 
-
 // сохранение имени и описания
 function changePopup() {
     // определяем значение полей ввода имени и описания
     let changeName = document.querySelector(".popup__input_text_name").value
     let changeDescription = document.querySelector(".popup__input_text_description").value
-
     // определяем поля куда вводим новые данные
     let name = document.querySelector(".profile__name");
     let description = document.querySelector(".profile__description");
-
-
     // проверка на введенный текст
     if (changeName == "") {
         // если текста нет, то ничего не происходит
     } else {
         // меняем имя
         name.innerHTML = changeName;
-
         // очищаем поля ввода имени
         document.querySelector(".popup__input_text_name").value = "";
-
         // меняем значение у поля ввода имени
         document.querySelector(".popup__input_text_name").value = changeName;
     }
-
     // проверка на введенный текст
     if (changeDescription == "") {
         // если текста нет, то ничего не происходит
     } else {
         // меняем описание
         description.innerHTML = changeDescription;
-
         // очищаем поля ввода описания
         document.querySelector(".popup__input_text_description").value = "";
-
         // меняем значение у поля ввода описания
         document.querySelector(".popup__input_text_description").value = changeDescription;
     }
-
     // закрываем попап
     closePopup()
 }
@@ -77,21 +67,19 @@ savePopupButton[0].addEventListener("click", changePopup, false);
 
 
 // лайк
-function clickLike() {
-    // определяем иконку лайк
-    let like = document.querySelector(".elements__like");
-
+// определяем иконки лайк
+var like = document.getElementsByClassName("elements__like");
+for (var i = 0; i < like.length; i++) {
+    // нажатие на лайк
+    like[i].addEventListener('click', likeButton);
+}
+function likeButton() {
     // делаем проверку на наличие класса актив
-    if (like.classList.contains("elements__active") === true) {
+    if (this.classList.contains('elements__active')) {
         // если класс есть, удаляем его
-        like.classList.remove("elements__active");
+        this.classList.remove('elements__active');
     } else {
         // если класса нет, добавляем его
-        like.classList.add("elements__active");
+        this.classList.add('elements__active');
     }
-
 }
-
-// нажатие на клик
-let likeButton = document.getElementsByClassName("elements__like");
-likeButton[0].addEventListener("click", clickLike, false);
