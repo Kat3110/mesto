bf5ece1b0320cbb7946c10209456cfbb7d43f5ad
Kokorina Ekaterina <kat3110@mail.ru>
2021-09-12 03:05:23 +0300

    hot fix

--- script.js
+++ script.js
@@ -1,28 +1,23 @@
 // показываем попап
 function tooglePopup() {
-    let popup = document.getElementsByClassName("popup__box");
-    let background = document.getElementsByClassName("popup__background");
+    let popup = document.getElementsByClassName("popup");
     popup[0].style.visibility = "visible";
-    background[0].style.visibility = "visible";
 }
 
 // кнопка + listener
 let button = document.getElementsByClassName("profile__edit");
 button[0].addEventListener("click", tooglePopup, false);
 
-let popup = document.getElementsByClassName("popup__box")[0];
-let background = document.getElementsByClassName("popup__background")[0];
+let popup = document.getElementsByClassName("popup")[0];
 
 // показываем попап
 function openPopup() {
     popup.style.visibility = "visible";
-    background.style.visibility = "visible";
 }
 
 // скрываем попап
 function closePopup() {
     popup.style.visibility = "hidden";
-    background.style.visibility = "hidden";
 }
 
 // кнопка открытия + listener
@@ -31,3 +26,13 @@ openPopupButton[0].addEventListener("click", openPopup, false);
 // кнопка закрытия + listener
 let closePopupButton = document.getElementsByClassName("popup__close");
 closePopupButton[0].addEventListener("click", closePopup, false);
+
+// // сохранение имени и описания
+function change() {
+    let changeName = document.querySelector(".popup__input_text_name").value
+    let changeDescription = document.querySelector(".popup__input_text_description").value
+    let name = document.querySelector(".profile__name");
+    let description = document.querySelector(".profile__description");
+    name.innerHTML = changeName;
+    description.innerHTML = changeDescription;
+}
